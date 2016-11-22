0374f53ebe40eaaa28b218b893d9a70e4a4ed099
WoodChipper <dgordon@dons-macbook.local>
2016-11-21 22:40:54 -0500

    Initial Commit

---
+++ Meds v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meds v2
+//
+//  Created by Don Gordon on 11/21/16.
+//  Copyright © 2016 DGsolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

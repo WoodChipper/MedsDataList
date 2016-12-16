dac4873e552721fd3dbc52cb8607d52f8230ac8b
WoodChipper <dgordon@Dons-MacBook.local>
2016-12-15 22:15:24 -0500

    First upload of particial project

---
+++ Meds v2/AddMedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddMedViewController.swift
+//  Meds v2
+//
+//  Created by Don Gordon on 11/21/16.
+//  Copyright © 2016 DGsolutions. All rights reserved.
+//
+
+import UIKit
+
+class AddMedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
